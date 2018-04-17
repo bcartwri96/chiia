4e199fbc69e6a381e1ccecf7d499a54f9019cd2b
bcartwri96 <bcartwri96@gmail.com>
2018-04-17 11:37:35 +1000

    change enviro vars so it runs as a script

--- enviro_vars.sh
+++ enviro_vars.sh
@@ -1,6 +1,6 @@
-# using this as the development variables needed. Run this once in dev to get
-# all the environment vars set up.
+#!/bin/bash
 export POSTGRES_URL="127.0.0.1:5432"
 export POSTGRES_USER="bencartwright"
 export POSTGRES_PW=""
 export POSTGRES_DB="chia-db"
+echo "Environment variables set."
