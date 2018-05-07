ee74a35055fbe916ed8e0177502b1f42d984f276
Anurag Vijay Kumar <u6320283@anu.edu.au>
2018-05-07 20:37:20 +1000

    anurags push front end

--- enviro_vars.sh
+++ enviro_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export SECRET_KEY="b'+\xa1\x9b\xa1\x7f\xc4!\x03\xe4\xf6p\xd0\x9cfvz\xd5\x00\x03\xf3\\|\xde\xb2!j[\xa2\x9b\x1ad\xbe\x99\xfeF\xf74o\xbd~q\xa7s\x91\x82\x83\xbe7'"
+export SECRET_KEY="b'\x8a\x82\x11b\xfe\xad\xb7\xb7<\xbd6\x0e;k\xda\x95=\xe8\xb3\xca\x1b@\xed{4\x975{\x1a:\x18*V}\xe0\x9b\x12'O/\xeb\xfb\x1e,V2\x0c\x1c'"
 export POSTGRES_URL="127.0.0.1:5432"
-export POSTGRES_USER="rajkamaldhull"
-export POSTGRES_PW=""
+export POSTGRES_USER="postgres"
+export POSTGRES_PW="test1234"
 export POSTGRES_DB="chia-db"
 echo "Environment variables set."
