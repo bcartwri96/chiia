311581fca0abcd598e4a7bdfd67de2e27b4241c7
anurag <u6320283@anu.edu.au>
2018-09-27 00:26:41 +1000

    correspondence options added to stage 3

--- code/forms.py
+++ code/forms.py
@@ -92,6 +92,8 @@ class stage3(wtf.FlaskForm):
     S3_date = DateField('S3_date', format='%Y', validators=[DataRequired("This date cannot be empty")])
     S3_reviews = IntegerField('S3_reviews', validators=[DataRequired("This column cannot be empty")])
     dataset_type = StringField('dataset_type', validators=[DataRequired("Please select dataset type")])
+    correspondence_info=StringField('correspondence_info', validators=[DataRequired("Please Enter the Information from correspondence ")])
+    correspondence_done=StringField('correspondence_done', validators=[DataRequired("Please enter whats been done to acquire this information ")])
 #    dataset_owner = StringField('dataset_owner', validators=[DataRequired("The dataset must have an owner")])
     task_submitted = SubmitField('Submit the Task')
 
